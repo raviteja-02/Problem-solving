8c83e6bae63d01e53651ab8f7325e1bb7e89fe61
raviteja-02 <raviteja-02@users.noreply.github.com>
2023-03-02 00:00:03 +0530

    question

--- Multiple.java
+++ Multiple.java
@@ -1,3 +1,14 @@
+/*Question 2
+Problem Statement –
+Capgemini in its online written test have a coding question, wherein the students are given a string with multiple characters that are repeated consecutively. You’re supposed to reduce the size of this string using mathematical logic given as in the example below :
+Input :
+aabbbbeeeeffggg
+Output:
+a2b4e4f2g3
+Input :
+abbccccc
+Output:
+ab2c5 */
 import java.util.*;
 
 public class Multiple {
